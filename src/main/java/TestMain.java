16ea9df2cb4f6a2aa75abc532c2050ce70f7f7b7
FelicityAmiao <1760077814@qq.com>
2019-08-07 22:19:48 +0800

    feat: add a frame with TestArea and a title

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,5 +1,26 @@
-public class TestMain {
-    public static void main(String[] args) {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+class First extends Frame {
+    First() {
+        TextArea text = new TextArea(5,20);
+        add(text);
+        setBounds(800, 340, 400, 400);
+        setTitle("Pretty Novel Note Book");
+        setLayout(new FlowLayout());//no layout manager
+        setVisible(true);//now frame will be visible, by default not visible
+
+        //set close window event listening
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    public static void main(String args[]) {
+        First f = new First();
     }
 }
